2f37199fb9ccd48f883e9f1b30cf6faa3e41305e
olliethedev <3martynov@gmail.com>
2026-01-08 11:08:02 -0500

    feat: add ui-builder comp + dependencies

---
+++ packages/ui/src/components/ui-builder/internal/form-fields/classname-control/index.tsx
@@ -0,0 +1,5 @@
+"use client";
+import { BreakpointClassNameControl } from "@workspace/components/ui/ui-builder/internal/form-fields/classname-control/breakpoint-classname-control";
+
+
+export default BreakpointClassNameControl;
